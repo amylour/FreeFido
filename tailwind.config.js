f9db8e76ffd644ed441f404f0f9276ddff96a741
AMY RICHARDSON <amyloumc@gmail.com>
2023-05-09 08:23:01 +0000

    Fix Tailwind configs and test in index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./templates/*.html"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
+  content: ["./templates/**/*.html,js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
 
