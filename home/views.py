a32b929df0394a007435dd7b4bb89ba14821c744
AMY RICHARDSON <amyloumc@gmail.com>
2023-05-17 21:18:08 +0000

    Setup userprofile app templates for login view

--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page """
     return render(request, 'home/index.html')
+
+
+def login_view(request):
+    """ A view to return the login page """
+    return render(request, 'home/login.html')
