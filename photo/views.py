c90cd4433d79bb37160ebb3eb2571920b1e734e8
AMY RICHARDSON <amyloumc@gmail.com>
2023-05-18 21:35:44 +0000

    setup: gallery page urls/views

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def gallery_view(request):
+    """ A view to return the gallery page """
+
+    # photos = Photos.objects.all()
+
+    return render(request, 'photo/gallery.html')
