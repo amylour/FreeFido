23672c867ec3e0352fd2ea5c96fe7d280824276e
AMY RICHARDSON <amyloumc@gmail.com>
2023-05-18 21:44:29 +0000

    Add find us page setup/urls/views

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('findus/', views.findus_view, name='findus'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,7 @@ def index(request):
     return render(request, 'home/index.html')
 
 
+def findus_view(request):
+    """ A view to return the find us page """
+    return render(request, 'home/findus.html')
+
