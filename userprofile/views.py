d2fad8ed7677804863b112c13a37374957bfdd14
AMY RICHARDSON <amyloumc@gmail.com>
2023-05-23 12:40:21 +0000

    add:create user form and view

--- userprofile/views.py
+++ userprofile/views.py
@@ -6,28 +6,28 @@ from .models import UserProfile, Dog
 def profile_view(request):
     """ A view to return the profile page """
 
-    user_profile = UserProfile.objects.get(user=request.user)
-    
-    return render(request, 'userprofile/profile.html', {'user_profile': user_profile})
+    # user_profile = UserProfile.objects.get(user=request.user)
+
+    return render(request, 'userprofile/profile.html')
+
+    # , {'user_profile': user_profile}
 
 
 def create_profile(request):
     """ Create profile for the user upon SignUp """
-    if request.method == "POST":
-        user_profile_form = UserProfileForm(request.POST, request.FILES)
+    if request.method == 'POST':
+        user_profile_form = UserProfileForm(request.POST)
         dog_form = DogForm(request.POST)
 
         if user_profile_form.is_valid() and dog_form.is_valid():
-            user_profile = user_profile_form.save(commit=FALSE)
+            # save the user's entered data
+            user_profile = user_profile_form.save(commit=False)
             user_profile.user = request.user
             user_profile.save()
-
-            dog = dog_form.save(commit=FALSE)
-            dog.owner = request.user
+            dog = dog_form.save(commit=False)
+            dog.user_profile = user_profile
             dog.save()
-
             return redirect('profile')
-
     else:
         user_profile_form = UserProfileForm()
         dog_form = DogForm()
