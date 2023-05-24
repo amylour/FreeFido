af33c27fbaa910cffa5c4245dcb43e051356fd85
AMY RICHARDSON <amyloumc@gmail.com>
2023-05-24 12:52:57 +0000

    edit:create_profile func/view profile page

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import CustomSignupView
+from .views import add_user, profile_view, create_profile
 
 urlpatterns = [
-    path('account/signup/', CustomSignupView.as_view(), name='account_signup'),
+    path('account/signup/', add_user.as_view(), name='account_signup'),
+    path('profile/', profile_view, name='profile'),
+    path('create_profile/', create_profile, name='create_profile'),
 ]
 
 
-# 
+#
 # from . import views
 
 
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,47 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from allauth.account.views import SignupView
-
-
-class CustomSignupView(SignupView):
-    template_name = 'account/signup.html'
-
-
-# from django.shortcuts import render, redirect
-# from .forms import UserProfileForm, DogForm
+# from django.contrib.auth.decorators import login_required
+from .models import UserProfile, Dog
+from .forms import UserProfileForm, DogForm
 # from .models import UserProfile, Dog
 
 
-# def profile_view(request):
-#     """ A view to return the profile page """
-
-#     # user_profile = UserProfile.objects.get(user=request.user)
-
-#     return render(request, 'userprofile/profile.html')
-
-#     # , {'user_profile': user_profile}
+class add_user(SignupView):
+    template_name = 'account/signup.html'
 
 
-# def create_profile(request):
-#     """ Create profile for the user upon SignUp """
-#     if request.method == 'POST':
-#         user_profile_form = UserProfileForm(request.POST)
-#         dog_form = DogForm(request.POST)
+# @login_required
+def profile_view(request):
+    """ A view to return the profile page """
+    # user_profile = UserProfile.objects.get(user=request.user)
+    # dog = Dog.objects.filter(owner=request.user)
+    # dog_breed_choices = dog.BREED_CHOICES
+    # context = {'user_profile': user_profile,
+    #            'dog': dog,
+    #            'dog_breed_choices': dog_breed_choices
+    #            }
+    return render(request, 'userprofile/profile.html')
+    # , context
 
-#         if user_profile_form.is_valid() and dog_form.is_valid():
-#             # save the user's entered data
-#             user_profile = user_profile_form.save(commit=False)
-#             user_profile.user = request.user
-#             user_profile.save()
-#             dog = dog_form.save(commit=False)
-#             dog.user_profile = user_profile
-#             dog.save()
-#             return redirect('profile')
-#     else:
-#         user_profile_form = UserProfileForm()
-#         dog_form = DogForm()
+# from django.shortcuts import render, redirect
 
-#     context = {
-#         'user_profile_form': user_profile_form,
-#         'dog_form': dog_form,
-#     }
-#     return render(request, 'create_profile.html', context)
+    # , {'user_profile': user_profile}
+    #
+
+
+def create_profile(request):
+    """ Create profile for the user upon SignUp """
+
+    user_profile_form = UserProfileForm(request.POST)
+    dog_form = DogForm(request.POST)
+
+    if user_profile_form.is_valid() and dog_form.is_valid():
+        # save the user's entered data
+        user_profile = user_profile_form.save(commit=False)
+        user_profile.user = request.user
+        user_profile.save()
+        dog = dog_form.save(commit=False)
+        dog.user_profile = user_profile
+        dog.save()
+        return redirect('profile')
+    else:
+        user_profile_form = UserProfileForm()
+        dog_form = DogForm()
+
+    context = {
+        'user_profile_form': user_profile_form,
+        'dog_form': dog_form,
+    }
+    return render(request, 'create_profile.html', context)
